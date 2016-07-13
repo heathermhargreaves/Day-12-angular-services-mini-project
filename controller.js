11e1c3adf692a868edecea1d761f2960e45c4fd1
heathermhargreaves <heathermhargreaves@gmail.com>
2016-07-13 12:36:50 -0600

    took out array, added API to populate data

--- controller.js
+++ controller.js
@@ -1,10 +1,12 @@
 angular.module('userProfiles').controller('MainController', function($scope, MainService){
 
-    $scope.getUsers = function(){
-        $scope.users = MainService.getUsers();
+    $scope.getUsers = function() {
+      mainService.getUsers().then(function(response) {
+        $scope.users = response.data.data;
+      });
     };
-
     $scope.getUsers();
+
 
     $scope.toggleFavorite = MainService.toggleFavorite;
 });
